e3ea3274e3ee34be71e72abc4565662af00a653c
Antonio Caminos <antoniocaminos@gmail.com>
2024-05-21 18:56:36 -0300

    agregado footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-/* saludo */
+// Función de saludo
 function holamunde() {
-    console.log("hola punto travel");    
+    console.log("Hola Punto Travel");
 }
-holamunde();    
+holamunde(); // Llama a la función de saludo
+
+// Inicialización del carrusel para que se desplace automáticamente
+
+const carrusel = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
+    
+$(document).ready(function() {
+    $('#carouselExampleFade').carousel({
+        interval: 3000 // Establece el intervalo de desplazamiento en milisegundos (en este caso, 3 segundos)
+    });
+});
